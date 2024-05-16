7d248dd7a572388d452b70d89307dc7dea64b7a6
Shan <shanganegoda96@gmail.com>
2024-05-16 17:39:56 +0800

    completed game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Card from './components/Card/Card';
 import './App.css'
 
@@ -6,30 +6,130 @@ import './App.css'
 
 function App() {
 
-const [wordsArray , setWordsArray] = useState(["Star","Flag","China","Red","Flag","Star","Red","China"]);
+  // const [wordsArray , setWordsArray] = useState(["Star","Flag","China","Red","Flag","Star","Red","China"]);
 
-const handleClick = i => {
-  console.log("clickedS")
-  setWordsArray((currentArray) => {
-    const obj = {...currentArray[i] , flipped : true }
-    const tempArray = currentArray.filter((_,index) => index != i)
-    console.log(tempArray)
-    return currentArray
-  })
-  // wordsArray[index].flipped =  !wordsArray[index].flipped
-}
+  const [wordsArray, setWordsArray] = useState(
+    [
+      {
+        word: "Zero",
+        flipped: false,
+        matched: false
+      },
+      {
+        word: "Star",
+        flipped: false,
+        matched: false
+      },
+      {
+        word: "China",
+        flipped: false,
+        matched: false
+      },
+      {
+        word: "Zero",
+        flipped: false,
+        matched: false
+      },
+      {
+        word: "Red",
+        flipped: false,
+        matched: false
+      },
+      {
+        word: "China",
+        flipped: false,
+        matched: false
+      },
+      {
+        word: "Red",
+        flipped: false,
+        matched: false
+      },
+      {
+        word: "Star",
+        flipped: false,
+        matched: false
+      },
+    ]);
 
-console.log(wordsArray)
+  const [clicks, setClicks] = useState(0);
+
+  const checkingArray = useRef([]);
+  const matchedArray = useRef([]);
+  const clickedIndex = useRef();
+
+  const handleClick = (i, word) => {
+
+    if (matchedArray.current.includes(word)) {
+      console.log("already matched");
+      return;
+    }
+
+    setClicks(currentClicks => {
+      return currentClicks + 1;
+    })
+
+    if (clickedIndex.current === i) {
+      console.log("clicked on same card");
+      return
+    }
+
+    if (clickedIndex.current == null) {
+      console.log(`prev index ${i} added`);
+      clickedIndex.current = i;
+    }
+
+    console.log(clickedIndex.current)
+
+    //Matched case
+    if (checkingArray.current.includes(word)) {
+      matchedArray.current.push(word);
+      clickedIndex.current = null;
+      checkingArray.current = []
+    }
+    //New click case
+    else if (checkingArray.current.length == 0) {
+      checkingArray.current.push(word);
+    }
+
+    setWordsArray((currentArray) => {
+      const tempObj = { ...currentArray[i], flipped: !currentArray[i].flipped }
+      const tempArray = [...currentArray.slice(0, i), tempObj, ...currentArray.slice(i + 1)]
+      return tempArray;
+    })
+
+    //Unmatched Case
+    if (!checkingArray.current.includes(word) && checkingArray.current.length) {
+      const previousIndex = clickedIndex.current;
+      // checkingArray.current.push(word);
+      setTimeout(() => {
+        setWordsArray((currentArray) => {
+          // Flip back both the previous and the current cards
+          const tempArray = currentArray.map((item, index) => {
+            if (index === i || index === previousIndex) {
+              return { ...item, flipped: false };
+            }
+            return item;
+          });
+          return tempArray;
+        });
+        clickedIndex.current = null;
+        checkingArray.current = [];
+      }, 1000);
+    }
+  }
 
   return (
     <div className='home'>
-      <div className="cardlist">
-        {wordsArray.map((obj,i) => <div key={i} onClick={() => handleClick(i)}>
-          <p>{obj.flipped ? "sds" : "ASdas"}</p>
-          <Card  word={obj.word} flipped={obj.flipped} />
-          </div>)
-          }
+      <div className="score">
+        <p className={{"color" : "white"}}>Clicks = {clicks}</p>
       </div>
+      <div className="cardlist">
+        {wordsArray.map((obj, i) =>
+          <Card key={i} word={obj.word} flipped={obj.flipped} onClick={() => handleClick(i, obj.word)} />
+        )}
+      </div>
+
     </div>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,12 @@
 import React, { useState } from 'react'
 import './Card.css'
 
-function Card({ word , onClick}) {
-
-    const [flipped , setFlipped] = useState(false);
-
-    const handleFlip = () => {
-        setFlipped(!flipped)
-    }
+function Card({ word , flipped ,  onClick}) {
 
     return (
-        <div className={`card ${flipped ? "flipped" : "" }`}>
+        <div className={`card ${flipped ? "flipped" : "" }`}  onClick={onClick}>
             <div className="inner">
                 <div className="front">
-                    
                 </div>
                 <div className="back">
                     <p>{word}</p>
